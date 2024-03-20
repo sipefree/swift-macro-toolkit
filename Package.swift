93e8703cc5e5123a3aeb02d4bd63c776b2adc304
Simon Free <sfree@apple.com>
2024-03-20 10:33:07 -0700

    Switched to use swift-syntax version 510.0.1.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-syntax.git",
-            from: "509.0.0"
+            from: "510.0.1"
         ),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
         .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "0.12.0"),
